eccaef69bb0f8a4af164572860ef2b515386f5e3
Zhixin MA <zhixinma97@gmail.com>
2019-01-21 17:23:21 +0800

    Update BinaryClassification.py

--- BinaryClassification.py
+++ BinaryClassification.py
@@ -4,20 +4,20 @@ def metric(output, label):
     pred = (output >= 0.5)
     truth = (label >= 0.5)
     right = pred.eq(truth)
-    acc = right.sum().double().item() / right.numel()
+    acc = right.sum().double().item() / right.numel()   # Accuracy
     TP = ( truth* right).sum().double().item()
     TN = (~truth* right).sum().double().item()
     FP = ( truth*~right).sum().double().item()
     FN = (~truth*~right).sum().double().item()
     Pnum = TP+FP
     Nnum = TN+FN
-    PR = Pnum / truth.numel()
-    TPR = TP/(TP+FN) if TP+FN > 0 else 0
-    FPR = FP/(TN+FP) if TN+FP > 0 else 0
-    Recall = TP/(TP+FN)
-    PPrecision = TP/Pnum if Pnum > 0 else 0
-    NPrecision = TN/Nnum if Nnum > 0 else 0
-    F1 = 2*TP/(2*TP+FP+FN)
+    PR = Pnum / truth.numel()                   # Positive Rate
+    TPR = TP/(TP+FN) if TP+FN > 0 else 0        # True Positive Rate
+    FPR = FP/(TN+FP) if TN+FP > 0 else 0        # False Positive Rate
+    Recall = TP/(TP+FN)                         # Recall
+    PPrecision = TP/Pnum if Pnum > 0 else 0     # Positive Precision
+    NPrecision = TN/Nnum if Nnum > 0 else 0     # Negative Precision
+    F1 = 2*TP/(2*TP+FP+FN)                      # F1 score
     return acc, TPR, FPR, F1, Recall, PPrecision, NPrecision, PR
 
 
